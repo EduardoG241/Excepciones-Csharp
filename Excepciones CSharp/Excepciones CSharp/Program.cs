dc2fb4bc586ba51bd741b90b81fa6f1e0abfee0e
EduardoG241 <105743710+EduardoG241@users.noreply.github.com>
2022-05-26 20:27:05 -0700

    Excepciones

---
+++ Excepciones CSharp/Excepciones CSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2;
+            int r;
+            try
+            {
+                Console.Write(" Ingrese el primer numero: ");
+                Num1 = int.Parse(Console.ReadLine());
+                Console.Write(" Ingrese el segundo numero: ");
+                Num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                r = Num1 / Num2;
+                Console.WriteLine(r);
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
